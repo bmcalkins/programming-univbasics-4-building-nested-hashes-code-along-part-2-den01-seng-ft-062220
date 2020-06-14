f1d57d99908d26e8b135133cc81c60409fc00e10
Bradley Calkins <b.raders1013@gmail.com>
2020-06-14 03:34:20 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -47,7 +47,7 @@ def add_characters
          ]
       }
    }
-
+epic_tragedy[:additional_characters] = []
 epic_tragedy[:additional_characters][0] = { name: "Prince Escalus" }
 epic_tragedy[:additional_characters][1] = { name: "Apothecary" }
 
